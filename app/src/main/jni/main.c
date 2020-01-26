a2446d95789dd62742e0741a02f051febb458f43
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-01-26 16:53:30 +0330

    glue will be loaded in android project instead of rust

--- app/src/main/jni/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-extern void rust_graphics_main(void* activity, void* saved_state, size_t saved_state_size);
-
-extern void ANativeActivity_onCreate(void* activity, void* saved_state, size_t saved_state_size) {
-    rust_graphics_main(activity, saved_state, saved_state_size);
-}
